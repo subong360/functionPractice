841132fe9189578781951520aa4012dc24cfbac6
subong360 <40824178+subong360@users.noreply.github.com>
2024-06-24 00:58:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function getFullNames(first, last) {
+  //return your full names are firstname and last name
+}
+
+
+console.log(getFullNames("samson","Aniekan"); // your full names are samson Aniekan
